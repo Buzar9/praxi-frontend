7c2a353bbac6496a3873d3f2f293a8e05929ea37
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-08-26 08:15:32 +0200

    Update endpoints.backend.js

---
+++ src/const/endpoints.backend.js
@@ -0,0 +1,4 @@
+export const ACHIEVEMENTS_URL = 'http://localhost:8080/achievements';
+export const REALIZATION_URL = 'http://localhost:8080/realizations'
+export const QUEST_URL = 'http://localhost:8080/quests';
+export const USER_URL = 'http://localhost:8080/users';
